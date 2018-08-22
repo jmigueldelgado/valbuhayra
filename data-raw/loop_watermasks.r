756279d7e7182ab46b96d02eeb5ec7c430bd2f84
JM Delgado <jmigueldelgado@gmail.com>
2018-08-22 09:01:26 +0200

    corrects CAV to numeric

--- data-raw/loop_watermasks.r
+++ data-raw/loop_watermasks.r
@@ -4,12 +4,9 @@ library(sf)
 library(jsonlite)
 
 cogerh=1
-head(CAV)
 
 library(ggplot2)
 cogerh
 
-xx=filter(CAV,reservatorio==cogerh) 
-
-plot(xx)
-ggplot(xx) + geom_point(aes(area,volume))
+filter(CAV,reservatorio==cogerh) %>%
+  ggplot(.) + geom_point(aes(area,volume))
--- data-raw/save_CAVs.r
+++ data-raw/save_CAVs.r
@@ -1,11 +1,14 @@
 library(readxl)
 library(dplyr)
 library(purrr)
-
+library(magrittr)
 # get CAVs from fUNCEME API and save them in data package
 library(jsonlite)
 request='http://api.funceme.br/rest/acude/referencia-cav?limit=0'
 cav=fromJSON(request)
 CAV=bind_cols(cav$list)
+
+CAV %<>% mutate_if(is.character,as.numeric)
+
 setwd('/home/delgado/proj/valbuhayra')
 save(CAV,file='data/cav.RData')
